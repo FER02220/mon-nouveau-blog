7895f77ea9e8c13824eea55d206e5a21dcd3c16c
assie <jl.assie@fertemis.fr>
2019-10-16 15:29:46 +0200

    Mon app Django Girls, premier commit

---
+++ NewRepository.bat
@@ -0,0 +1,6 @@
+echo "# mon-nouveau-blog" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/fertemis/mon-nouveau-blog.git
+git push -u origin master
